68336b571f8035e04c7a5ca32053965dc1fe0707
AndreKleef <kleef001@gmail.com>
2020-07-13 15:49:41 +0400

    Tonaeto: 10/10 страниц с мобилкой

--- js/script.js
+++ js/script.js
@@ -1,139 +1,66 @@
 
-$(document).on('click', '.custom-input-number .cin-increment', function(e) {
-  let $input = $(this).siblings('.cin-input'),
-     val = parseInt($input.val()),
-     max = parseInt($input.attr('max')),
-     step = parseInt($input.attr('step'));
+//INPUT FILE
+var inputs = document.querySelectorAll( '.inputfile, .input-1, .input-2, .input-3' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+  var label  = input.nextElementSibling,
+    labelVal = label.innerHTML;
 
-  let temp = val + step;
-  $input.val(temp <= max ? temp : max);
-  // console.log(temp);
- });
- $(document).on('click', '.custom-input-number .cin-decrement', function(e) {
-  let $input = $(this).siblings('.cin-input'),
-     val = parseInt($input.val()),
-     min = parseInt($input.attr('min')),
-     step = parseInt($input.attr('step'));
+  input.addEventListener( 'change', function( e )
+  {
+    var fileName = '';
+    if( this.files && this.files.length > 1 )
+      fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+    else
+      fileName = e.target.value.split( '\\' ).pop();
 
-  let temp = val - step;
-  $input.val(temp >= min ? temp : min);
-
-  // console.log(temp);
+    if( fileName )
+      label.querySelector( 'span' ).innerHTML = fileName;
+    else
+      label.innerHTML = labelVal;
+  });
 });
 
+
+//TABS
+let tab = function () {
+  let tabNav = document.querySelectorAll('.registration-nav__item'),
+      tabContent = document.querySelectorAll('.tab'),
+      tabName;
+
+      tabNav.forEach(item => {
+        item.addEventListener('click', selectTabNav)
+      });
+
+  function selectTabNav() {
+    tabNav.forEach(item => {
+        item.classList.remove('is-active');
+    });
+    this.classList.add('is-active');
+    tabName = this.getAttribute('data-tab-name');
+    selectTabContent(tabName);
+  }
+
+  function selectTabContent(tabName) {
+    tabContent.forEach(item => {
+        item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
+    })
+  }
+};
+tab();
+
+//STARS
 $(document).ready(function () {
   $(".star label").click(function(){
-    $(this).parent().find("label").css({"color": "#F7DB39"});
+    $(this).parent().find("label").css({"color": "#9e9e9e"});
     $(this).css({"color": "#F7DB39"});
     $(this).nextAll().css({"color": "#F7DB39"});
     $(this).css({"background-color": "transparent"});
     $(this).nextAll().css({"background-color": "transparent"});
   });
-
-
-  $('.promo__slider').owlCarousel({
-    animateOut: 'slideOutDown',
-    animateIn: 'pulse',
-    loop:true,
-    nav:true,
-    dots: true,
-    responsive:{
-        0:{
-            items:1
-        }
-    }
-  });
-
-  $('.category-slider').owlCarousel({
-    loop:true,
-    nav:false,
-    dots: true,
-    margin: 30,
-    responsive:{
-      0:{
-        items:1,
-        stagePadding: 0
-      },
-      421:{
-        items:1,
-        stagePadding: 60
-      },
-      550:{
-        items:1,
-        stagePadding: 100
-      },
-      769:{
-        items:1,
-        stagePadding: 250
-      },
-      993:{
-        items:2,
-        stagePadding: 250
-      },
-      1201:{
-        items:3,
-        stagePadding: 250
-      },
-      1441:{
-        items:5,
-        stagePadding: 200
-      }
-    }
-  });
-
-  if(window.innerWidth < 992) {
-    $('.arrivals__flex').owlCarousel({
-      nav:false,
-      margin: 20,
-      dots: false,
-      responsive:{
-          0:{
-              items:1,
-              stagePadding: 0
-          },
-          576:{
-              items:1,
-              stagePadding: 50
-          },
-          769:{
-              items:2
-          }
-      }
-    });
-  }
-
-  if($("#map").length>0) {
-    ymaps.ready(function() {
-        var myMap = new ymaps.Map("map",{
-            center: [53.23699207119478,63.57197599999994],
-            zoom: 14.5,
-
-            "multiTouch": true,
-            controls: []
-        });
-        var myPlacemark = new ymaps.Placemark([53.23699207119478,63.57197599999994],{
-            hintContent: 'Россия, 410028, Энгельс, ул. Улица, д.9'
-        },{
-            iconLayout: ymaps.templateLayoutFactory.createClass('<div class="point"><img src="img/icon.svg"></div>'),
-            iconPane: 'overlaps',
-            iconSize: [32, 32],
-            iconOffset: [-16, -32]
-        });
-        myMap.geoObjects.add(myPlacemark);
-        myMap.behaviors.disable('scrollZoom')
-    });
-  }
-
-  $(".star label").click(function(){
-    $(this).parent().find("label").css({"color": "#9e9e9e"});
-    $(this).css({"color": "#FF8400"});
-    $(this).nextAll().css({"color": "#FF8400"});
-    $(this).css({"background-color": "transparent"});
-    $(this).nextAll().css({"background-color": "transparent"});
-  });
-
 });
 
+//DROPDOWN
 (function($, window, document, undefined) {
 
   'use strict';
@@ -165,19 +92,17 @@ $(document).ready(function () {
 
  })(jQuery, window, document);
 
-let basketButton = document.querySelector('.page-header__block'),
-    basketModal = document.querySelector('.page-header__basket-modal'),
 
-    background = document.querySelector('.modal__background'),
+//EVENTS
+let background = document.querySelector('.modal__background'),
     modalCancel = document.querySelectorAll('.modal-window-cancel'),
-    buttonLiner = document.querySelectorAll('.cruises__table-button'),
     modalWindow = document.querySelector('.modal-window'),
-
-    menuСontent = document.querySelector('.page-header__bottom-button'),
-    navMenu = document.querySelector('.promo__menu'),
 
     buttonFilter = document.querySelector('.news-content__filter-button'),
     contentFilter = document.querySelector('.news-content__feedback'),
+
+    buttonLk = document.querySelector('.lk__flex-button'),
+    contentLk = document.querySelector('.lk__left'),
 
     burgerButton = document.querySelector('.burger'),
     burgerMenu = document.querySelector('.page-header__nav');
@@ -187,13 +112,17 @@ burgerButton.addEventListener('click', function() {
   burgerMenu.classList.toggle('openmenu');
 });
 
+if (!buttonLk) {
+  buttonLk = null;
+} else {
+  buttonLk.addEventListener('click', function() {
+    contentLk.classList.toggle('open-menu');
+  });
+};
+
 buttonFilter.addEventListener('click', function() {
   buttonFilter.classList.toggle('active');
   contentFilter.classList.toggle('opencontent');
-});
-
-menuSort.addEventListener('click', function() {
-  navSort.classList.toggle('openmenu');
 });
 
 linkPersonal.forEach(el => {
@@ -202,77 +131,14 @@ linkPersonal.forEach(el => {
   });
 });
 
-buttonCruise.forEach(el => {
-  el.addEventListener('click', function() {
-    background.classList.toggle('visible');
-    modalCruise.classList.toggle('open-cruise');
-  });
-});
-
-buttonLiner.forEach(el => {
-  el.addEventListener('click', function() {
-    background.classList.toggle('visible');
-    modalWindow.classList.toggle('open-window');
-  });
-});
-
 if (!background) {
   background = null;
 } else {
-    background.addEventListener('click', function() {
+  background.addEventListener('click', function() {
     background.classList.remove('visible');
     modalWindow.classList.remove('open-window');
   });
 };
-
-// let clickButton = document.querySelectorAll('.button-feedback'),
-//     modalCancel = document.querySelector('.modal-window-cancel'),
-//     modalWindow = document.querySelector('.modal-window'),
-//     background = document.querySelector('.modal__background');
-
-// clickButton.forEach(el => {
-//   el.addEventListener('click', function() {
-//     background.classList.toggle('visible');
-//     modalWindow.classList.toggle('open-window');
-//   });
-// });
-
-// background.addEventListener('click', function() {
-//   background.classList.remove('visible');
-//   modalWindow.classList.remove('open-window');
-// });
-// modalCancel.addEventListener('click', function() {
-//   background.classList.remove('visible');
-//   modalWindow.classList.remove('open-window');
-// });
-
-
-// let coll = document.getElementsByClassName("collapsible");
-// let i;
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     let content = this.nextElementSibling;
-//     if (content.style.maxHeight){
-//       content.style.maxHeight = null;
-//     } else {
-//       content.style.maxHeight = content.scrollHeight + "px";
-//     }
-//   });
-// };
-
-
-var dateSelect = $('.flight-datepicker');
-
-dateSelect.datepicker({
-  autoclose: true,
-  language: "ru",
-  maxViewMode: 0,
-  startDate: "now"
-}).on('change', function() {
-  var start = $.format.date(dateSelect.find('.start-date').datepicker('getDate'));
-  var end = $.format.date(dateSelect.find('.end-date').datepicker('getDate'));
-});
 
 
 
